2c8f524f522055d49eb841d01f6df592500709c2
chafikilyes <gouasmia.chafik@gmail.com>
2020-09-21 16:32:09 +0100

    commit for home code getting :)

--- src/app/list-resto/list-resto.component.ts
+++ src/app/list-resto/list-resto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {RestoService} from '../resto.service'
 @Component({
   selector: 'app-list-resto',
   templateUrl: './list-resto.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListRestoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private resto :RestoService) { }
 
   ngOnInit(): void {
+
+    this.resto.getList();
   }
 
 }
